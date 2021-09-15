4fd8cd29ead934a4243b226665a77304bf3c37ae
ccaioadriano <caiobhadriano@gmail.com>
2021-09-15 00:31:24 -0300

    Primeiro commit - projeto criado

---
+++ src/test/java/com/workshopMongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workshopMongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
