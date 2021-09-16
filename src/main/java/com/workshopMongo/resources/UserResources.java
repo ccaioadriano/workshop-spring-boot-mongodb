b73724d75389a97c3de23a3d44a3864ebc964c88
ccaioadriano <caiobhadriano@gmail.com>
2021-09-16 11:53:00 -0300

    Obtendo Usuário por Id

--- src/main/java/com/workshopMongo/resources/UserResources.java
+++ src/main/java/com/workshopMongo/resources/UserResources.java
@@ -5,6 +5,7 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,5 +27,13 @@ public class UserResources {
 		List<User> list = serviceUser.findAll();
 		List<UserDTO> listDTO = list.stream().map(user -> new UserDTO(user)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDTO);
+	}
+	
+	@RequestMapping(value="/{id}", method = RequestMethod.GET)
+	public ResponseEntity<UserDTO> findbyId(@PathVariable String id) {
+		
+		User userById = serviceUser.findById(id);
+
+		return ResponseEntity.ok().body(new UserDTO(userById));
 	}
 }
--- src/main/java/com/workshopMongo/services/UserService.java
+++ src/main/java/com/workshopMongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.workshopMongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.workshopMongo.domain.User;
 import com.workshopMongo.repositories.UserRepository;
+import com.workshopMongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,10 @@ public class UserService {
 	public List<User> findAll() {
 		
 		return userRepository.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> user = userRepository.findById(id);
+		return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 }
