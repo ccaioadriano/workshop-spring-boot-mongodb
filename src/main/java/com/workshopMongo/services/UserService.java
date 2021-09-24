4cc0e5cf9cfe8a8bfe16258f5494a709ec2f6e48
ccaioadriano <caiobhadriano@gmail.com>
2021-09-24 11:20:54 -0300

    Inserção de usuários com POST

--- src/main/java/com/workshopMongo/services/UserService.java
+++ src/main/java/com/workshopMongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.workshopMongo.domain.User;
+import com.workshopMongo.dto.UserDTO;
 import com.workshopMongo.repositories.UserRepository;
 import com.workshopMongo.services.exception.ObjectNotFoundException;
 
@@ -15,13 +16,32 @@ public class UserService {
 	@Autowired
 	UserRepository userRepository;
 	
+	/*
+	 * Retorna todos os Usuários
+	 * */
 	public List<User> findAll() {
 		
 		return userRepository.findAll();
 	}
 	
+	/*
+	 * Procura usuários através do ID
+	 * */
 	public User findById(String id) {
 		Optional<User> user = userRepository.findById(id);
 		return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
+	
+	/*
+	 * Insere um usuário no banco de dados
+	 * */
+	public User insert(User obj) {
+		return userRepository.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+	}
+	
+	
 }
